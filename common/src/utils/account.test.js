d0d338ffa4c349e29ac6d78e9ecec8521dd7cc66
Jaeyoung Jung <jaeyoung@medibloc.org>
2018-08-29 20:58:22 +0900

    Fix eslint error

--- common/src/utils/account.test.js
+++ common/src/utils/account.test.js
@@ -5,7 +5,7 @@ describe('Utils: Account', () => {
   describe('extractPublicKey', () => {
     it('should return a Hex string from any given string', () => {
       const passphrase = 'field organ country moon fancy glare pencil combine derive fringe security pave';
-      const publicKey = '032dd8c5bcb41457d76de9a1145e8e220c765c776fdadd3f68e68c7b4baee81e6f';
+      const publicKey = '02cd94dab39ac0530331b855de5af5abddc6776692e52346dbffb40a14c186337c';
       expect(extractPublicKey(passphrase)).to.be.equal(publicKey);
     });
   });
@@ -13,13 +13,13 @@ describe('Utils: Account', () => {
   describe('extractAddress', () => {
     it('should return the account address from given passphrase', () => {
       const passphrase = 'field organ country moon fancy glare pencil combine derive fringe security pave';
-      const derivedAddress = '032dd8c5bcb41457d76de9a1145e8e220c765c776fdadd3f68e68c7b4baee81e6f';
+      const derivedAddress = '02cd94dab39ac0530331b855de5af5abddc6776692e52346dbffb40a14c186337c';
       expect(extractAddress(passphrase)).to.be.equal(derivedAddress);
     });
 
     it('should return the account address from given public key', () => {
-      const publicKey = '03422b50476f97327384b2a0f6a65112ced47751bd3ff45d1572e8fad540a3d4c7';
-      const derivedAddress = '03422b50476f97327384b2a0f6a65112ced47751bd3ff45d1572e8fad540a3d4c7';
+      const publicKey = '02cd94dab39ac0530331b855de5af5abddc6776692e52346dbffb40a14c186337c';
+      const derivedAddress = '02cd94dab39ac0530331b855de5af5abddc6776692e52346dbffb40a14c186337c';
       expect(extractAddress(publicKey)).to.be.equal(derivedAddress);
     });
   });
--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
       {
         test: /\.svg$/,
         exclude: [/fonts/],
-        loader: 'svg-inline-loader'
+        loader: 'svg-inline-loader',
       },
       {
         test: /\.png$/,
