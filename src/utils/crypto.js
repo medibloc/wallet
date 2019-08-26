1fe28cb1b10166a30090e334c2c996483cb56056
ggomma <ggomma@gmail.com>
2019-08-26 16:48:54 +0900

    Use own encrypt code not using old medjs

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,5 +1,41 @@
-import { cryptography } from 'medjs';
+import {
+  createHash, randomBytes,
+  createCipheriv, createDecipheriv,
+} from 'crypto-browserify';
+import is from 'is_js';
 
-export const decryptData = (password, encData) => cryptography.decryptData(password, encData);
+const algorithm = 'AES-256-CTR';
 
-export const encryptData = (password, data) => cryptography.encryptData(password, data);
+export const decryptData = (password, encData) => {
+  if (!encData) return null;
+
+  const textParts = encData.split(':');
+  if (textParts.length !== 2) {
+    throw new Error('Invalid encrypted data format');
+  }
+
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encText = Buffer.from(textParts[0], 'hex');
+  const hashedPassword = createHash('sha256').update(password).digest('hex');
+
+  const decipher = createDecipheriv(algorithm, Buffer.from(hashedPassword, 'hex'), iv);
+  const decryptedData = decipher.update(encText, 'hex', 'utf8');
+
+  try {
+    return decryptedData + decipher.final('utf8');
+  } catch (err) {
+    throw new Error('Wrong password');
+  }
+};
+
+export const encryptData = (password, data) => {
+  if (!is.string(password)) throw new Error('Password needs to be a string format');
+
+  const iv = randomBytes(16);
+  const hashedPassword = createHash('sha256').update(password).digest('hex');
+
+  const cipher = createCipheriv(algorithm, Buffer.from(hashedPassword, 'hex'), iv);
+  const encryptedData = `${cipher.update(data, 'utf8', 'hex')}${cipher.final('hex')}`;
+
+  return `${iv.toString('hex')}:${encryptedData}`;
+};
