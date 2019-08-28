cc7d6545e96ebd60461e094aa0bba4ad42f95978
ggomma <ggomma@gmail.com>
2019-08-28 11:49:00 +0900

    Set account's asset balance on header

--- src/components/organisms/header/header.js
+++ src/components/organisms/header/header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-// import Countdown from 'react-countdown-now';
 import AccountVisual from '../../atoms/accountVisual/index';
 import Box from '../../atoms/box/index';
-// import CountDownTemplate from './countDownTemplate';
 import CopyToClipboard from '../../atoms/copyToClipboard/index';
 import MedAmount from '../../atoms/medAmount/index';
 import PrivateWrapper from '../../atoms/privateWrapper/index';
 import styles from './header.css';
-// import CustomCountDown from './customCountDown';
-import { addMed } from '../../../utils/med';
+import { mulMed } from '../../../utils/med';
+import parseBalance from '../../../utils/balanceParser';
 
 class Header extends React.Component {
   render() {
     const { account } = this.props;
+    const balances = parseBalance(account);
+
     return (
       <header className={`${styles.wrapper} mainHeader`}>
         <div className={`${styles.loginInfo}`}>
@@ -22,38 +22,14 @@ class Header extends React.Component {
                 <div className={`account ${styles.account}`}>
                   <div className={styles.information} align="right">
                     <div className={styles.total}>
-                      <MedAmount roundTo={2}
-                        val={addMed(addMed(account.balance, account.staking), account.unstaking)} />
+                      <MedAmount roundTo={6}
+                        val={mulMed(balances.total, 10 ** 6)} />
                       <h5>MED</h5>
                     </div>
                     <CopyToClipboard
                       value={account.address}
                       className={`${styles.address} account-information-address`}
                       copyClassName={styles.copy}/>
-                    { /* !this.props.autoLog && account.passphrase ?
-                      <div className={styles.unlocked}>{this.props.t('Unlocked')}</div> : <div/>}
-                    {this.props.autoLog ? <div className={styles.timer}>
-                      {((account.expireTime &&
-                          account.expireTime !== 0) &&
-                          account.passphrase) ?
-                        <div>
-                          <Countdown
-                            date={account.expireTime}
-                            renderer={CountDownTemplate}
-                            onComplete={() => {
-                              this.props.removeSavedAccountPassphrase();
-                            }
-                            }
-                          ><CustomCountDown
-                              resetTimer={this.props.resetTimer}
-                              autoLog={this.props.autoLog}
-                              t={this.props.t}
-                            />
-                          </Countdown>
-                        </div> : <div/>}
-                    </div>
-                      : <div/>
-                    */ }
                   </div>
                   <Box className={styles.avatar}>
                     <AccountVisual
