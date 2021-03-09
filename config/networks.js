7d320bf49725c64c99ede4d53226e9c0533a604a
Youngjoon Lee <yjlee@medibloc.org>
2021-03-09 23:07:16 +0900

    fix: Typo in networks.js (#20)

--- config/networks.js
+++ config/networks.js
@@ -4,7 +4,7 @@ const networks = {
     chainId: 'panacea-1',
     code: 0,
     mServerURL: 'https://explorer-server.medibloc.org',
-    mClientURL: 'https://explroer.medibloc.org',
+    mClientURL: 'https://explorer.medibloc.org',
     nodes: ['https://wallet-lcd.gopanacea.org'],
   },
   testnet: { // network name translation t('Testnet');
